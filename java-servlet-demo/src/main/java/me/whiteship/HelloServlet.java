9ce539c071f6b157ceeb0f120fee1d41d05663f3
Shin <Shin@Shin-PC>
2019-05-28 00:21:00 +0900

    리스너, 필터 오류나지만 커밋함.. 오류수정요함

--- java-servlet-demo/src/main/java/me/whiteship/HelloServlet.java
+++ java-servlet-demo/src/main/java/me/whiteship/HelloServlet.java
@@ -15,17 +15,24 @@ public class HelloServlet extends HttpServlet {
     }
 
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
 		System.out.println("doGet");
+		
 		resp.getWriter().println("<html>");
 		resp.getWriter().println("<head>");
 		resp.getWriter().println("<body>");
-		resp.getWriter().println("<h1>Hello Servlet</h1>");
+		resp.getWriter().println("<h1>Hello" + getName() +"</h1>");
 		resp.getWriter().println("</body>");
 		resp.getWriter().println("</head>");
 		resp.getWriter().println("</html>");
 	}
+
 	@Override
 	public void destroy() {
 		System.out.println("destroy");
+	}
+	
+	private Object getName() {
+		return getServletContext().getAttribute("name");
 	}
 }
