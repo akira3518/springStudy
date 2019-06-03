6311f01801a406340307be06e04889a1054b2e1f
Shin <Shin@Shin-PC>
2019-06-04 00:03:49 +0900

    money 프로젝트 생성

---
+++ tddMoneyTest/src/test/java/com/test/moneyTest/testHomeController.java
@@ -0,0 +1,14 @@
+package com.test.moneyTest;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations= {"file:src/main/webapp/WEB-INF/spring/appServlet/servlet-context.xml"})
+@WebAppConfiguration
+public class testHomeController {
+	
+	  
+}
